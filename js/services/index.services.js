dcd564f7b470c6c210d40a683f8a3941d24e3818
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-08-28 12:52:36 +0300

    Added HTML tags to display country information on the page.

--- js/services/index.services.js
+++ js/services/index.services.js
@@ -3,3 +3,20 @@
 function searchCountry() {
   document.querySelector('.search-country').textContent = 'searchCountry'
 }
+
+function showCountryData(countryData) {
+  const cnamge = document.querySelector('.country-name')
+  const cimg = document.querySelector('.country-img')
+  const cpopulation = document.querySelector('.country-population')
+  const carea = document.querySelector('.country-area')
+
+  const name = countryData.name.common
+  const flagImage = countryData.flags.png
+  const population = countryData.population.toLocaleString()
+  const area = countryData.area.toLocaleString() + ' km²'
+
+  cnamge.textContent = name
+  cimg.src = flagImage
+  cpopulation.textContent = population
+  carea.textContent = area
+}
