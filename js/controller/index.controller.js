b7bec9b409c0d0e08aab543b0e3a47d461da6c38
nhftkfr25 <168108819+nhftkfr25@users.noreply.github.com>
2024-08-28 11:40:47 +0300

    Added modal dialog to display detailed information about the searched country.

--- js/controller/index.controller.js
+++ js/controller/index.controller.js
@@ -3,3 +3,73 @@
 function onInit() {
   menuRender()
 }
+
+function openShowModal(countryData) {
+  const nativeNames = Object.values(countryData.name.nativeName)
+    .map((n) => `${n.official} (${n.common})`)
+    .join(', ')
+  const currency = Object.values(countryData.currencies)[0]
+  const languages = Object.values(countryData.languages).join(', ')
+  const borders = countryData.borders ? countryData.borders.join(', ') : 'None'
+  const population = countryData.population.toLocaleString()
+  const flagImage = countryData.flags.png
+
+  document.querySelector('.dialog').innerHTML = `
+      <div class="dialog-content">
+        <span class="close" onclick="closeDialog()" data-i18n="close">&times;</span>
+        <img class="dialog-img" src="${flagImage}" alt="${countryData.name.common}" />
+        <div class="dialog-info">
+          <h2 data-i18n="countryName">${countryData.name.common} (${countryData.name.official})</h2>
+          <table>
+            <tr>
+              <td data-i18n="nativeName">Native Names</td>
+              <td>${nativeNames}</td>
+            </tr>
+            <tr>
+              <td data-i18n="capital">Capital</td>
+              <td>${countryData.capital.join(', ')}</td>
+            </tr>
+            <tr>
+              <td data-i18n="region">Region</td>
+              <td>${countryData.region}, ${countryData.subregion}</td>
+            </tr>
+            <tr>
+              <td data-i18n="population">Population</td>
+              <td>${population}</td>
+            </tr>
+            <tr>
+              <td data-i18n="languages">Languages</td>
+              <td>${languages}</td>
+            </tr>
+            <tr>
+              <td data-i18n="currency">Currency</td>
+              <td>${currency.name} (${currency.symbol})</td>
+            </tr>
+            <tr>
+              <td data-i18n="area">Area</td>
+              <td>${countryData.area.toLocaleString()} km²</td>
+            </tr>
+            <tr>
+              <td data-i18n="borders">Borders</td>
+              <td>${borders}</td>
+            </tr>
+            <tr>
+              <td data-i18n="timezone">Timezones</td>
+              <td>${countryData.timezones.join(', ')}</td>
+            </tr>
+            <tr>
+              <td data-i18n="gini">Gini Index (2016)</td>
+              <td>${countryData.gini ? countryData.gini[2016] : 'N/A'}</td>
+            </tr>
+          </table>
+        <button onclick="closeDialog()" class="close-btn" data-i18n="close">Close</button>
+        </div>
+      </div>`
+
+  document.querySelector('.dialog').style.display = 'flex'
+}
+
+function closeDialog() {
+  const elDialog = document.querySelector('.dialog')
+  elDialog.style.display = 'none'
+}
--- js/services/index.services.js
+++ js/services/index.services.js
@@ -1,5 +1,20 @@
 'use strict'
 
 function searchCountry() {
-  document.querySelector('.search-country').textContent = 'searchCountry'
+  let elCountry = document.querySelector('#cname').value
+
+  getAns(elCountry, openShowModal)
+}
+
+function getAns(name, cb) {
+  const xhr = new XMLHttpRequest()
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const ans = JSON.parse(xhr.responseText)[0]
+      console.log(ans)
+      cb(ans)
+    }
+  }
+  xhr.open('GET', `https://restcountries.com/v3.1/name/${name}?fullText=true`, true)
+  xhr.send()
 }
